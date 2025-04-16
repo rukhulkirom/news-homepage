2cec152cde32567398809ba05e702fa8db93ed2a
rukhulkirom <muhamadrukhulkirom@gmail.com>
2025-04-16 19:03:03 +0700

    javascript setup

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import "./scss/style.scss";
+
+const btnOpen = document.querySelector("#btn-open");
+const media = window.matchMedia("(width < 69.375em)");
+
+const openMobileMenu = () => {
+  console.log("Open menu");
+
+  btnOpen.setAttribute("aria-expanded", "true");
+};
+
+const setupNav = (e) => {
+  if (e.matches) {
+    // is mobile
+    console.log("is mobile");
+  } else {
+    // is desktop
+    console.log("is desktop");
+  }
+};
+
+btnOpen.addEventListener("click", openMobileMenu);
+
+media.addEventListener("change", function (e) {
+  setupNav(e);
+});
