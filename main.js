53c5dd18c9cb0c7a644328d8cd4ffcc07b415729
rukhulkirom <muhamadrukhulkirom@gmail.com>
2025-04-16 20:19:02 +0700

    accessibility features

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./scss/style.scss";
 
 const body = document.querySelector("body");
+const main = document.querySelector("main");
 const btnOpen = document.querySelector("#btn-open");
 const btnClose = document.querySelector("#btn-close");
 const media = window.matchMedia("(width < 69.375em)");
@@ -12,6 +13,9 @@ const openMobileMenu = () => {
 
   btnOpen.setAttribute("aria-expanded", "true");
   body.classList.add("noscroll");
+  navContent.removeAttribute("inert");
+  main.setAttribute("inert", "");
+  btnClose.focus();
 };
 
 const closeMobileMenu = () => {
@@ -19,12 +23,17 @@ const closeMobileMenu = () => {
 
   btnOpen.setAttribute("aria-expanded", "false");
   body.classList.remove("noscroll");
+  navContent.setAttribute("inert", "");
+  main.removeAttribute("inert");
+  btnOpen.focus();
 };
 
 const setupNav = (e) => {
   if (e.matches) {
     // is mobile
     console.log("is mobile");
+
+    navContent.setAttribute("inert", "");
 
     setTimeout(() => {
       navContent.style.display = "block";
@@ -33,6 +42,10 @@ const setupNav = (e) => {
   } else {
     // is desktop
     console.log("is desktop");
+
+    navContent.removeAttribute("inert");
+    main.removeAttribute("inert");
+
     navContent.style.display = "";
   }
 };
@@ -45,4 +58,10 @@ btnClose.addEventListener("click", closeMobileMenu);
 
 media.addEventListener("change", function (e) {
   setupNav(e);
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "Tab") {
+    console.log(document.activeElement);
+  }
 });
